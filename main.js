4fd2ca150dc3e13e6d702ff2746942eaf13b367e
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-12-31 13:47:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+song1="";
+song2="";
+function preload(){
+  song1=loadSound("Aankh Marey - Simmba 128 Kbps.mp3");
+  song2=loadSound("Jai Jai Shivshankar - war 128 Kbps.mp3");
+}
+
 function setup(){
   canvas=createCanvas(500,500);
   canvas.center();
   video=createCapture(VIDEO);
   video.hide();
 }
-function draw() {
+function draw() {  
     image(video,0,0,500,500);
 }
