ec556a42c94aee10be1bcaae98668d80275d4606
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2021-01-02 12:44:14 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
   song1=loadSound("Aankh Marey - Simmba 128 Kbps.mp3");
   song2=loadSound("Jai Jai Shivshankar - war 128 Kbps.mp3");
@@ -10,6 +14,25 @@ function setup(){
   canvas.center();
   video=createCapture(VIDEO);
   video.hide();
+  
+  poseNet=ml5.posenet(video,modelloaded);
+  poseNet.on('pose',gotPoses);
+}
+function modelloaded(){
+  console.log("PoseNet Initialized!");
+}
+function gotPoses(results){
+  if(results.length > 0){
+    console.log(results);
+
+    leftWristX=results[0].pose.leftWrist.x;
+    leftWristY=results[0].pose.leftWrist.y;
+    console.log("Left Wrist X = "+leftWristX+" , "+"Left Wrist Y = "+leftWristY);
+
+    rightWristX=reuslts[0].pose.rightWrist.x;
+    rightWristY=results[0].pose.rightWrist.y;
+    console.log("Right Wrist X = "+rightWristX+" , "+"Right Wrist Y = "+rightWristY);
+  }
 }
 function draw() {  
     image(video,0,0,500,500);
